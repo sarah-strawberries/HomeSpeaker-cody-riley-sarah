40323e95c08f2cfb318d0ac94fe6ea1998f46d6b
Derpipose <thefluffmaster2000@gmail.com>
2025-02-25 16:16:33 -0700

    Added the base pages

---
+++ HomeSpeaker.BlazorMaui/Views/Queue.xaml.cs
@@ -0,0 +1,9 @@
+namespace HomeSpeaker.BlazorMaui.Views;
+
+public partial class Queue : ContentPage
+{
+	public Queue()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HomeSpeaker.BlazorMaui/Views/Streams.xaml.cs
@@ -0,0 +1,9 @@
+namespace HomeSpeaker.BlazorMaui.Views;
+
+public partial class Streams : ContentPage
+{
+	public Streams()
+	{
+		InitializeComponent();
+	}
+}
