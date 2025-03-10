7b7ef0aab6caa0277c23dc8c3baec5a3ec427090
Cody-Howell <codyhowell44@gmail.com>
2025-03-09 18:04:46 -0600

    Starting work on Streams

--- HomeMaui/ViewModels/StreamsVM.cs
+++ HomeMaui/ViewModels/StreamsVM.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace HomeMaui.ViewModels {
-    public class StreamsVM {
-    }
+namespace HomeMaui.ViewModels;
+public partial class StreamsVM : ObservableObject {
+
 }
